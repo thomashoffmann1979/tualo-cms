9c1c66364b8c2b665dabe985bc12aed1fd76b7a6
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2014-01-24 10:47:57 +0100

    cleaup code

--- routes/cms.js
+++ routes/cms.js
@@ -2,47 +2,67 @@ var path = require('path'),
     cms;
 
 var ui = function(req, res, next) {
+    
     var template = 'simple',
         MainNavigation = [],
         urlParts = req.originalUrl.split('/'),
         queryPage = urlParts[1];
+    
     if (queryPage == ''){
         queryPage = 'home';
     }
+    
+    res.locals.Template = 'simple';
+    res.locals.MainNavigation = cms.mainNavigation;
+    
+    res.locals.Page = {
+        ID: -1,
+        Title: 'Error',
+        Content: 'Not found',
+        ClassName: 'ErrorPage'
+    };
+
     cms.once('error',function(err){
-        res.render(path.join(template,'pages/single'),{
-            Site: {
-                Title: cms.config.Title,
-                Tagline: cms.config.Tagline,
-            },
-            Page: {
-                Title: 'Error',
-                Content: JSON.stringify(err),
-            },
-            MainNavigation: MainNavigation
-        });
+        res.locals.Page = {
+            Title: 'Error',
+            Content: JSON.stringify(err),
+            ClassName: 'ErrorPage'
+        };
+        if (typeof err == 'string'){
+            res.locals.Page.Content = err;
+        }
+        console.log('error',queryPage);
+        return sendResult(req,res,next);
     });
     
     cms.once('navigation', function(id,navigation){
-        MainNavigation = navigation;
-        cms.get(queryPage);
+        if (id == res.locals.Page.ID){
+            res.locals.Navigation = navigation;
+            return sendResult(req,res,next);
+        }
     });
     
     cms.once('page',function(page){
-        res.render(path.join(template,'pages/single'),{
-            Site: {
-                Title: cms.config.Title,
-                Tagline: cms.config.Tagline,
-            },
-            Page: {
-                Title: page.Title,
-                Content: page.Content,
-            },
-            MainNavigation: MainNavigation
-        });
+        console.log('page',queryPage,page.ID);
+        res.locals.Page = {
+            ID: page.ID,
+            Title: page.Title,
+            Content: page.Content,
+            ClassName: page.ClassName
+        };
+        cms.navigation(page.ID);
     });
+    cms.get(queryPage);
+    return;
     
-    cms.navigation(0);
+}
+
+var sendResult = function(req,res,next){
+    res.locals.Site = {
+        Title: cms.config.Title,
+        Tagline: cms.config.Tagline,
+    };
+    return res.render(path.join(res.locals.Template,'pages',res.locals.Page.ClassName));
 }
 
 exports.route = function(app){
